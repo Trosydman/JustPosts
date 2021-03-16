5775bef14f79825f03130d43e9fb8c7253a930d5
Carlos Mesquita <carlos.mesquita.cm2@gmail.com>
2021-03-16 23:39:30 +0100

    Implement PostRepository to get post in  paginated format

---
+++ app/src/main/java/com/cmesquita/technicaltest/justposts/repository/PostRepository.kt
@@ -0,0 +1,28 @@
+package com.cmesquita.technicaltest.justposts.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import com.cmesquita.technicaltest.justposts.data_source.remote.GraphQLZeroRemoteDataSource
+import com.cmesquita.technicaltest.justposts.data_source.remote.api.graphqlzero.mapper.PostsResponseDTOMapper
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
+
+class PostRepository(
+    private val graphQLZeroRemoteDataSource: GraphQLZeroRemoteDataSource,
+    private val postsResponseDTOMapper: PostsResponseDTOMapper,
+    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
+) {
+    suspend fun getPosts() =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                PostPagingSource(graphQLZeroRemoteDataSource, postsResponseDTOMapper)
+            }
+        ).flow
+            .flowOn(defaultDispatcher)
+}
