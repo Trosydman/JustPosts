e89a96cdbde73b5f8afb4e52b744b01bab071781
Carlos Mesquita <carlos.mesquita.cm2@gmail.com>
2021-03-16 23:38:15 +0100

    Create PostPagingSource to paginate the query request

---
+++ app/src/main/java/com/cmesquita/technicaltest/justposts/repository/PostPagingSource.kt
@@ -0,0 +1,39 @@
+package com.cmesquita.technicaltest.justposts.repository
+
+import androidx.paging.PagingSource
+import com.apollographql.apollo.exception.ApolloException
+import com.cmesquita.technicaltest.justposts.data_source.remote.GraphQLZeroRemoteDataSource
+import com.cmesquita.technicaltest.justposts.data_source.remote.api.graphqlzero.mapper.PostsResponseDTOMapper
+import com.cmesquita.technicaltest.justposts.ui.model.Post
+import kotlinx.coroutines.flow.first
+
+class PostPagingSource(
+    private val graphQLZeroRemoteDataSource: GraphQLZeroRemoteDataSource,
+    private val postsResponseDTOMapper: PostsResponseDTOMapper
+) : PagingSource<Int, Post>() {
+
+    companion object {
+        const val FIRST_PAGE_INDEX = 1
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Post> {
+        val position = params.key ?: FIRST_PAGE_INDEX
+
+        return try {
+            val postsResponseDTO = graphQLZeroRemoteDataSource.getPosts(position, params.loadSize)
+                .first()
+            val posts: List<Post> = postsResponseDTOMapper.mapToDomainModel(postsResponseDTO)
+                ?: throw Exception("Posts are null")
+
+            LoadResult.Page(
+                data = posts,
+                prevKey = if (position == FIRST_PAGE_INDEX) null else position - 1,
+                nextKey = if (posts.isEmpty()) null else position + 1
+            )
+        } catch (exception: ApolloException) {
+            LoadResult.Error(exception)
+        } catch (exception: Exception) {
+            LoadResult.Error(exception)
+        }
+    }
+}
